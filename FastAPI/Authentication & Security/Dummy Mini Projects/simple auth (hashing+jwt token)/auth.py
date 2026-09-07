cba56e703269b9ac78b32eb4750d8ef4b1ba5503
bsparrow49 <aashishchaudhari249@gmail.com>
2026-09-07 17:39:01 +0545

    feat(simple_auth): added basic auth that does encryption and decryption

---
+++ FastAPI/Authentication & Security/Dummy Mini Projects/simple auth (hashing+jwt token)/auth.py
@@ -0,0 +1,24 @@
+from passlib.context import CryptContext
+from jose import jwt,JWTError
+from datetime import datetime,timedelta,timezone
+
+SECRET_KEY="dummy_secret_key"
+ALGORITHM="HS256"
+
+pwd_context=CryptContext(schemes=["bcrypt"],deprecated="auto")
+
+def hash_password(password:str)->str:
+    return pwd_context.hash(password)
+
+def create_access_token(data:dict,expires_delta:timedelta=timedelta(seconds=30)):
+    to_encode=data.copy()
+    expire=datetime.now(timezone.utc)+expires_delta
+    to_encode.update({"exp":expire})
+    return jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
+
+def decode_access_token(token:str)->dict:
+    try:
+        return jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
+    except JWTError:
+        return None
+
