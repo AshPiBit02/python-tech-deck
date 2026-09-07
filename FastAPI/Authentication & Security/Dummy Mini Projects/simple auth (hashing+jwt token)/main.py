fc2df5f281dbf95764528d98268c31dd1427ba9f
bsparrow49 <aashishchaudhari249@gmail.com>
2026-09-07 18:03:25 +0545

    fix(routes): resolve in field db access errors

--- FastAPI/Authentication & Security/Dummy Mini Projects/simple auth (hashing+jwt token)/main.py
+++ FastAPI/Authentication & Security/Dummy Mini Projects/simple auth (hashing+jwt token)/main.py
@@ -1,20 +1,26 @@
 from fastapi import FastAPI,APIRouter,Depends,HTTPException
-from .database import USERS
-from .auth import hash_password
+from database import USERS
+from auth import hash_password,verify_password,create_access_token
 
 app=FastAPI()
 
 @app.post("/register")
 def register(email:str,password:str)->dict:
-    if USERS[email]:
+    if email in USERS:
         raise HTTPException(status_code=400,detail="Email already in use!")
     hashed_passsword=hash_password(password)
-    USERS["email"]=email
-    USERS["password"]=hashed_passsword
+    USERS[email]={"email":email,"password":hashed_passsword}
     return {"message":f"New user {email} registered"}
 
-@app.post("/token")
-
-
-
+@app.post("/login")
+def login(email:str,password:str):
+    if not USERS[email]:
+        raise HTTPException(status_code=401,detail=f"Invalid username, register first")
     
+    if not verify_password(password,USERS[email]["password"]):
+        raise HTTPException(status_code=401,detail=f"Incorrect password!")
+    
+    access_token=create_access_token({"sub":email})
+    return {"access_token":access_token,"token_type":"bearer"}
+    
+
