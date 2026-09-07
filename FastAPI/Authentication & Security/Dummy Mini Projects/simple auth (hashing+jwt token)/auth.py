d7ee778d029ec63a078a053bc72dec6c0de89687
bsparrow49 <aashishchaudhari249@gmail.com>
2026-09-07 17:50:07 +0545

    feat(auth): aaded password verifier

--- FastAPI/Authentication & Security/Dummy Mini Projects/simple auth (hashing+jwt token)/auth.py
+++ FastAPI/Authentication & Security/Dummy Mini Projects/simple auth (hashing+jwt token)/auth.py
@@ -10,6 +10,9 @@ pwd_context=CryptContext(schemes=["bcrypt"],deprecated="auto")
 def hash_password(password:str)->str:
     return pwd_context.hash(password)
 
+def verify_password(plain_paasword:str,hashed_password:str)->bool:
+    return pwd_context.verify(plain_paasword,hashed_password)
+
 def create_access_token(data:dict,expires_delta:timedelta=timedelta(seconds=30)):
     to_encode=data.copy()
     expire=datetime.now(timezone.utc)+expires_delta
