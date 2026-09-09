2008ff3fa91fff0f9f9df58bab1bb27307e67ba3
bsparrow49 <aashishchaudhari249@gmail.com>
2026-09-09 21:28:05 +0545

    feat(oauth2_password_flow): added confirm password field and no need to pass the user_id to access payload

--- FastAPI/Authentication & Security/OAuth2/Core Samples/oauth2_password_flow.py
+++ FastAPI/Authentication & Security/OAuth2/Core Samples/oauth2_password_flow.py
@@ -1,20 +1,22 @@
 from fastapi import Depends,HTTPException,Header,FastAPI
 from fastapi.security import OAuth2PasswordRequestForm,OAuth2PasswordBearer
-from root_auth import hash_password,create_access_token,verify_password
+from root_auth import hash_password,create_access_token,verify_password,decode_access_token
 
 app=FastAPI()
 oauth2_scheme=OAuth2PasswordBearer(tokenUrl="token")
 
 USERS:dict[str,dict]={}
 
-def get_user(user_id:str=Header(...),token:str=Depends(oauth2_scheme)):
-    user=USERS.get(user_id)
-    if user is None:
-        raise HTTPException(status_code=400,detail=f"User with id {user_id} not registered")
-    return user
+def get_user(token:str=Depends(oauth2_scheme)):
+    payload=decode_access_token(token)
+    if not payload:
+        raise HTTPException(status_code=401,detail="Invalid or expired token")
+    return payload
 
 @app.post("/register")
-def register_user(email:str=Header(...),password:str=Header(...),role:str=Header(...)):
+def register_user(email:str=Header(...),password:str=Header(...),confirm_password:str=Header(...),role:str=Header(...)):
+    if password!=confirm_password:
+        raise HTTPException(status_code=400,detail="Password didn't match")
     for user in USERS.values():
         if user["email"]==email:
             raise HTTPException(status_code=400,detail="Email already registered")
@@ -28,12 +30,14 @@ def login(form_data:OAuth2PasswordRequestForm=Depends()):
     user = next((u for u in USERS.values() if u["email"]==form_data.username),None)
     if user is None or not verify_password(form_data.password,user["password"]):
         raise HTTPException(status_code=401,detail="Invalid email or password")
-    access_token=create_access_token({"sub":user["email"]})
+    access_token=create_access_token({"sub":user["email"],"role":user["role"]})
     return {"access_token":access_token,"token_type":"bearer"}
 
 @app.get("/me")
 def get_me(user:dict=Depends(get_user)):
     return user
+
+
 
     
 
