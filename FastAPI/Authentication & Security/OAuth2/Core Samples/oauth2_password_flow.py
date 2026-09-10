aed275479603f57ab64844c3d80881ec8f528ac3
bsparrow49 <aashishchaudhari249@gmail.com>
2026-09-10 11:40:35 +0545

    feat(oauth2_password_flow): allows admin to register new users while being logged in

--- FastAPI/Authentication & Security/OAuth2/Core Samples/oauth2_password_flow.py
+++ FastAPI/Authentication & Security/OAuth2/Core Samples/oauth2_password_flow.py
@@ -11,7 +11,7 @@ USERS:dict[str,dict]={}
 def block_if_logged_in(token:str=Depends(optional_oauth2)):
     if token:
         payload=decode_access_token(token)
-        if payload:
+        if payload and payload.get("role")!="admin":
             raise HTTPException(status_code=403,detail="Registration disabled for logged-in users")
 
 def get_user(token:str=Depends(oauth2_scheme)):
@@ -22,13 +22,13 @@ def get_user(token:str=Depends(oauth2_scheme)):
     info=USERS[user_id]["about"]
     return {"user_id":user_id,"email":payload.get("sub"),"role":payload.get("role"),"about":info}
 
-def get_users_list(current_user:dict=Depends(get_user)):
+def users_list(current_user:dict=Depends(get_user)):
     if current_user["role"]!="admin":
         raise HTTPException(status_code=403,detail="Access Denied")
     return {uid: {k:v for k,v in u.items() if k!="password"} for uid, u in USERS.items()}
 
 @app.post("/register")
-def register_user(email:str=Header(...),password:str=Header(...),confirm_password:str=Header(...),role:str=Header(...),about:str|None=None):
+def register_user(email:str=Header(...),password:str=Header(...),confirm_password:str=Header(...),role:str=Header(...),about:str|None=None,_:None=Depends(block_if_logged_in)):
     if password!=confirm_password:
         raise HTTPException(status_code=400,detail="Password didn't match")
     for user in USERS.values():
@@ -52,5 +52,5 @@ def get_me(user:dict=Depends(get_user)):
     return user
 
 @app.get("/user/list")
-def get_users_list(users:dict=Depends(get_users_list)):
+def get_users_list(users:dict=Depends(users_list)):
     return users
