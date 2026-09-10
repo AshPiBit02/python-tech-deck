42c01e3c36b6e5685578677d7932b0ca67f7c432
bsparrow49 <aashishchaudhari249@gmail.com>
2026-09-10 18:50:53 +0545

    refactor(root_auth): distinguish access and refresh token to reduce security flow

--- FastAPI/Authentication & Security/OAuth2/Core Samples/root_auth.py
+++ FastAPI/Authentication & Security/OAuth2/Core Samples/root_auth.py
@@ -26,6 +26,9 @@ def decode_access_token(token:str)->dict:
         return None
 
 def create_refresh_token(data:dict,expires_delta:timedelta=timedelta(days=7)):
-    return create_access_token(data,timedelta(days=7))
+    to_encode=data.copy()
+    expires=datetime.now(timezone.utc)+expires_delta
+    to_encode.update({"exp":expires})
+    return jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
 
 
