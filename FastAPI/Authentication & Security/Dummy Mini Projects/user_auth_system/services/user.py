01e85c798bab189b21866a03f4f3d1bcaa2581dd
bsparrow49 <aashishchaudhari249@gmail.com>
2026-09-12 23:21:49 +0545

    feat(user service): added register and update user services

--- FastAPI/Authentication & Security/Dummy Mini Projects/user_auth_system/services/user.py
+++ FastAPI/Authentication & Security/Dummy Mini Projects/user_auth_system/services/user.py
@@ -12,3 +12,29 @@ def get_user_by_id(db:Session,user_id:int)->User|None:
 def get_all_users(db:Session)->list[User]:
     return db.query(User).all()
 
+def create_user(db:Session,user_in:UserRegistration)->User:
+    hash_password=hash_password(user_in.password)
+    db_user=User(
+        email=user_in.email,
+        hash_password=hash_password,
+        role=user_in.role,
+        )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def update_user(db:Session,user_id:int,user_in:UserUpdate)->User:
+    db_user=get_user_by_id(db,user_id)
+    if not db_user:
+        return None
+
+    update_data=user_in.model_dump(exclude_unset=True)
+    for key,value in update_data.items():
+        if key=="password":
+            setattr(db_user,"hashed_password",hash_password(value))
+        else:
+            setattr(db_user,key,value)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
