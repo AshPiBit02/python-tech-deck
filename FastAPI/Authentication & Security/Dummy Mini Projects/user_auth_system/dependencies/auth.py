0d16ed5f455aead17263ed98641dbbec826afbfc
bsparrow49 <aashishchaudhari249@gmail.com>
2026-09-13 00:10:12 +0545

    feat(auth): added admin requiredment dependency

--- FastAPI/Authentication & Security/Dummy Mini Projects/user_auth_system/dependencies/auth.py
+++ FastAPI/Authentication & Security/Dummy Mini Projects/user_auth_system/dependencies/auth.py
@@ -19,6 +19,11 @@ def get_current_user(token:str=Depends(oauth2_scheme),db:Session=Depends(get_db)
         raise HTTPException(status_code=401,detail="User not found")
     return db_user
 
+def require_admin(current_user:User=Depends(get_current_user))->User:
+    if current_user.role!=Role.admin:
+        raise HTTPException(status_code=403,detail="Access Denied")
+    return current_user
+
 def verify_admin_key(admin_key:str)->None:
     if admin_key!=settings.admin_secret_key:
         raise ValueError("Invalid admin key!")
