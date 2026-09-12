8fb601cefad571c5dde28ed70901b5cefa8a8eb4
bsparrow49 <aashishchaudhari249@gmail.com>
2026-09-12 20:35:20 +0545

    refactor(user model): removed additional role for simplicity and added db and server level enum

--- FastAPI/Authentication & Security/Dummy Mini Projects/user_auth_system/models/user.py
+++ FastAPI/Authentication & Security/Dummy Mini Projects/user_auth_system/models/user.py
@@ -3,14 +3,12 @@ from sqlalchemy import (Column,INTEGER,String,func,DateTime)
 from enum import Enum as PyEnum
 from sqlalchemy.types import Enum
 class Role(str,PyEnum):
-    super_admin="Super Admin"
     admin="Admin"
     user="User"
-    guest="Guest"
 class User(Base):
     __tablename__="users"
     id=Column(INTEGER,primary_key=True,index=True)
     email=Column(String(255),unique=True,nullable=False)
     hashed_password=Column(String(128),nullable=False)
-    role=Column(Enum(Role),nullable=False)
+    role=Column(Enum(Role),nullable=False,default=Role.user,server_default=Role.user.value)
     created_at=Column(DateTime,server_default=func.now())
