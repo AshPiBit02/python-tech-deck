5b22de21e18cda9bc94eabc675a49971a3c7f855
bsparrow49 <aashishchaudhari249@gmail.com>
2026-09-13 16:12:25 +0545

    feat(user router): added read_me,list_users, update user and remove user routers

--- FastAPI/Authentication & Security/Dummy Mini Projects/user_auth_system/routers/users.py
+++ FastAPI/Authentication & Security/Dummy Mini Projects/user_auth_system/routers/users.py
@@ -3,3 +3,26 @@ from dependencies.db_dependency import database_dependency
 from models import User
 from schemas.user import UserOut,UserUpdate
 from services.user import get_all_users,get_user_by_id,update_user,delete_user
+from dependencies.auth import get_current_user,require_admin
+
+router=APIRouter(tags=["users"])
+
+@router.get("/me",response_model=UserOut)
+def read_me(current_user:User=Depends(get_current_user)):
+    return current_user
+
+@router.patch("/me",response_model=UserOut)
+def update_me(db:database_dependency,body:UserUpdate,current_user:User=Depends(get_current_user)):
+    updated_user=update_user(db,current_user.id,body)
+    return update_user
+
+@router.get("/admin/users",response_model=list[UserOut])
+def list_users(db:database_dependency,admin_user:User=Depends(require_admin)):
+    return get_all_users(db)
+
+@router.delete("/admin/users/{user_id}")
+def remove_user(db:database_dependency,user_id:int,admin_user:User=Depends(require_admin)):
+    deleted=delete_user(db,user_id)
+    if not deleted:
+        raise HTTPException(status_code=404,detail="User not found")
+    return deleted
