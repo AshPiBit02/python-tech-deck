a2bc67c66bbd50eac587bf2ac1eefe21f4fa50e1
bsparrow49 <aashishchaudhari249@gmail.com>
2026-09-15 23:10:09 +0545

    test(test_login): added test method for login by non existing user and token claims correctness check

--- FastAPI/Authentication & Security/Dummy Mini Projects/user_auth_system/tests/test_login.py
+++ FastAPI/Authentication & Security/Dummy Mini Projects/user_auth_system/tests/test_login.py
@@ -17,4 +17,25 @@ def test_login_wrong_password(client,registered_user):
     })
     assert response.status_code==401
 
-    
+
+def test_login_nonexistent_user(client):
+    response=client.post("/token",data={
+        "username":"unknown@gmail.com",
+        "password":"nomatterwhat",
+    })
+    assert response.status_code==401
+
+def test_login_token_has_correct_claims(client,registered_user):
+    from jose import jwt  
+    from core.config import settings
+
+    reponse=client.post("/token",data={
+        "username":"testuser@gmail.com",
+        "password":"strongpass",
+    })
+    access_token=reponse.json()["access_token"]
+    payload=jwt.decode(access_token,settings.secret_key,algorithms=[settings.algorithm])
+
+    assert payload["type"]=="bearer"
+    assert payload["sub"]==str(registered_user.id)
+    assert payload["role"]=="User"
