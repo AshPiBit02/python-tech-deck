82ae670c47f5854dd57cfeaa52e146dd8315541b
bsparrow49 <aashishchaudhari249@gmail.com>
2026-09-15 22:49:13 +0545

    test(test_register.py): added test method admin register without admin secret key and with wrong secret key

--- FastAPI/Authentication & Security/Dummy Mini Projects/user_auth_system/tests/test_register.py
+++ FastAPI/Authentication & Security/Dummy Mini Projects/user_auth_system/tests/test_register.py
@@ -39,3 +39,24 @@ def test_register_weak_password(client):
         "confirm_password":"ned",
     })
     assert response.status_code==422
+
+def test_register_admin_without_key(client):
+    respone=client.post("/register",json={
+        "email":"fakeadmin@gmail.com",
+        "password":"dummypassword",
+        "confirm_password":"dummypassword",
+        "role":"Admin",
+    })
+    assert respone.status_code==422
+
+def test_register_admin_with_wrong_key(client):
+    response=client.post("/register",json={
+        "email":"imposteradmin@gmail.com",
+        "password":"maybecorrect",
+        "confirm_password":"maybecorrect",
+        "role":"Admin",
+        "admin_secret_key":"wrongadminkey",
+    })
+    assert response.status_code==403
+
+
