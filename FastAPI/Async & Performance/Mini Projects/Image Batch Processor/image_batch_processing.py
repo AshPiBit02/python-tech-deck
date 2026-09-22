dd8b4ae2503ab7235ca54cffe6f584961a32c76f
bsparrow49 <aashishchaudhari249@gmail.com>
2026-09-22 19:43:29 +0545

    feat(image_batch_processing): added resize_image_cpu_bound that resize image via cpu bound also calculates processig duration

--- FastAPI/Async & Performance/Mini Projects/Image Batch Processor/image_batch_processing.py
+++ FastAPI/Async & Performance/Mini Projects/Image Batch Processor/image_batch_processing.py
@@ -18,3 +18,12 @@ THUMBNAIL_DIR.mkdir(exist_ok=True)
 JOBS:dict[str,dict]={}
 
 THUMBNAIL_SIZE=(200,200)
+
+def resize_image_cpu_bound(source_path:Path,dest_path:Path)->dict:
+    start=time.perf_counter()
+    with Image.open(source_path) as img:
+        img.thumbnail(THUMBNAIL_SIZE)
+        img.save(dest_path)
+    elapsed=time.perf_counter()-start
+    return {"duration_seconds":round(elapsed,3),"thumbnail_path":str(dest_path)}
+
