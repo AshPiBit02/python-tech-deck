c1662ae0022fbde00eade13914af2ddf0f82962f
bsparrow49 <aashishchaudhari249@gmail.com>
2026-09-23 17:53:23 +0545

    feat(health_checker): added async method that checks single api dependency

--- FastAPI/Async & Performance/Mini Projects/Concurrent Multi-API Health Checker/health_checker.py
+++ FastAPI/Async & Performance/Mini Projects/Concurrent Multi-API Health Checker/health_checker.py
@@ -12,3 +12,23 @@ DEPENDENCIES={
     "fact_api":"https://catfact.ninja/fact",
     "definitely_down":"https://this-domain-does-not-exists-xyz123.com",
 }
+
+async def check_one(client:httpx.AsyncClient,name:str,url:str)->dict:
+    start=time.perf_counter()
+    try:
+        response=await client.get(url,timeout=5)
+        elapsed=time.perf_counter()-start
+        return {
+            "name":name,
+            "status":"up" if response.status_code<400 else "degraded",
+            "http_status":response.status_code,
+            "reponse_time_seconds":round(elapsed,3),
+        }
+    except Exception as e:
+        elapsed=time.perf_counter()-start
+        return{
+            "name":name,
+            "status":"down",
+            "error":str(e),
+            "response_time_seconds":round(elapsed,3),
+        }
