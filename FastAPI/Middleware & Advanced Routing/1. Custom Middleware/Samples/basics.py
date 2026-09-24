281ee2d039cecee16d060f832b98e0ae88b59af1
bsparrow49 <aashishchaudhari249@gmail.com>
2026-09-24 17:36:50 +0545

    samaple(basics): a tiny bit more useful sample that counts requests

--- FastAPI/Middleware & Advanced Routing/1. Custom Middleware/Samples/basics.py
+++ FastAPI/Middleware & Advanced Routing/1. Custom Middleware/Samples/basics.py
@@ -2,13 +2,28 @@ from fastapi import FastAPI
 
 app=FastAPI()
 
+request_count=0
+
+"""
 @app.middleware("http")
 async def say_hello(request,call_next):
     print("A request just arrived!")
     response=await call_next(request)
     print("A response is about to be sent back!")
     return response
-
+"""
+@app.middleware("http")
+async def count_requests(request,call_next):
+    global request_count
+    request_count+=1
+    print(f"This is request number {request_count}")
+    response=await call_next(request)
+    return response
+    
 @app.get("/")
 def home():
     return {"message":"Halo boss"}
+
+@app.get("/about")
+def about():
+    return {"message":"This is for basic demonstration"}
